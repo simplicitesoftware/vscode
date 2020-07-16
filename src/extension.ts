60d8033b918ab67c6f4363112e9b8cee05ee6fff
David AZOULAY <dazoulay@simplicite.fr>
2020-07-16 12:03:28 +0200

    Configurations

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,16 @@ import * as vscode from 'vscode';
 import * as Simplicite from 'simplicite';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Simplicite tools activated');
+	let cfg = vscode.workspace.getConfiguration('simplicite');
+	let url = cfg.get("url");
+	let username = cfg.get("username");
+	console.log(`Simplicite tools activated for URL ${url} and username ${username}`);
 
-	let app = Simplicite.session({ url: 'https://demo.dev.simplicite.io', username: 'website', password: 'simplicite' });
+	let app = Simplicite.session({ url: url, username: username, password: cfg.get("password") });
 
 	let disposable = vscode.commands.registerCommand('simplicite.info', () => {
 		app.getAppInfo().then((info: any) => {
-			vscode.window.showInformationMessage(info.title + ' ' + info.platformversion);
+			vscode.window.showInformationMessage(`${info.title} ${info.platformversion} (${url})`);
 		});
 	});
 
