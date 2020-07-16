41641e4a53a3bcb9eb1c9df121f9574b489b5c48
David AZOULAY <dazoulay@simplicite.fr>
2020-07-16 22:46:15 +0200

    FS milestone

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,16 @@ import * as Simplicite from 'simplicite';
 import { SimpliciteFS } from './SimpliciteFS';
 
 export function activate(context: vscode.ExtensionContext) {
-	let cfg = vscode.workspace.getConfiguration('simplicite');
-	let url = cfg.get("url");
-	let username = cfg.get("username");
+	let cfg: vscode.WorkspaceConfiguration = vscode.workspace.getConfiguration('simplicite');
+	let url: string|undefined = cfg.get("url");
+	let username: string|undefined = cfg.get("username");
+	let modules: string[]|undefined = cfg.get('modules');
+
 	console.log(`Simplicite tools activated for URL ${url} and username ${username}`);
 
-	let app = Simplicite.session({ url: url, username: username, password: cfg.get("password"), debug: true });
-	const mdl = app.getBusinessObject('Module');
+	let app: any = Simplicite.session({ url: url, username: username, password: cfg.get("password"), debug: false });
+	const mdl: any = app.getBusinessObject('Module');
+	const obj: any= app.getBusinessObject('ObjectInternal');
 
 	context.subscriptions.push(vscode.commands.registerCommand('simplicite.appinfo', () => {
 		app.getAppInfo().then((info: any) => {
@@ -25,26 +28,46 @@ export function activate(context: vscode.ExtensionContext) {
 		});
 	}));
 
-	const fs = new SimpliciteFS();
+	const fs: SimpliciteFS = new SimpliciteFS();
 
 	context.subscriptions.push(vscode.workspace.registerFileSystemProvider('simplicite', fs, { isCaseSensitive: true }));
 
-	context.subscriptions.push(vscode.commands.registerCommand('simplicite.initWorkspace', () => {
+	function refreshModules() {
+		app.debug(modules);
+		let mf: string = '\'Demo\''; // TODO: use modules configuration
+		mdl.search({ mdl_name: `in (${mf})` }).then((ms: any) => {
+			app.debug(JSON.stringify(ms));
+			for (let i: number = 0; i < ms.length; i++) {
+				const m: any = ms[i];
+				const uri: string = `simplicite:/${m.mdl_name}/`;
+				fs.createDirectory(vscode.Uri.parse(uri));
+				fs.createDirectory(vscode.Uri.parse(`${uri}/src`));
+				fs.writeFile(vscode.Uri.parse(`${uri}/README.md`), Buffer.from(m.mdl_comment || ''), { create: true, overwrite: true });
+				refreshBusinessObjects(m.row_id, m.mdl_name, uri);
+			}
+		});
+	}
+
+	function refreshBusinessObjects(mdlId: number, mdlName: string, mdlUri: string) {
+		obj.search({ row_module_id: mdlId }, { inlineDocuments: [ 'obo_script_id' ] }).then((os: any) => {
+			app.debug(JSON.stringify(os));
+			for (let i: number = 0; i < os.length; i++) {
+				const o: any = os[i];
+				console.log(JSON.stringify(o));
+				const d: any = o.obo_script_id;
+				if (d) {
+					fs.writeFile(vscode.Uri.parse(`${mdlUri}/src/${d.name}`), Buffer.from(d.content, 'base64'), { create: true, overwrite: true });
+				}
+			}
+		});
+	}
+
+	context.subscriptions.push(vscode.commands.registerCommand('simplicite.refresh', refreshModules));
+
+	context.subscriptions.push(vscode.commands.registerCommand('simplicite.init', () => {
 		app.getAppInfo().then((info: any) => {
 			app.debug(JSON.stringify(info));
 			vscode.workspace.updateWorkspaceFolders(0, 0, { uri: vscode.Uri.parse('simplicite:/'), name: info.title });
-		});
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('simplicite.refresh', () => {
-		mdl.search({ mdl_name: 'not in (\'System\', \'Interface\')' }).then((modules: any) => {
-			app.debug(JSON.stringify(modules));
-			for (let i = 0; i < modules.length; i++) {
-				const module = modules[i];
-				const moduleURI = `simplicite:/${module.mdl_name}/`;
-				fs.createDirectory(vscode.Uri.parse(moduleURI));
-				fs.writeFile(vscode.Uri.parse(`${moduleURI}/README.md`), Buffer.from(module.mdl_comment || ''), { create: true, overwrite: true });
-			}
 		});
 	}));
 }
