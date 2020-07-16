41bcc67da274df10d5075215e82b7767b0b1c836
David AZOULAY <dazoulay@simplicite.fr>
2020-07-16 21:21:48 +0200

    FS milestone

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as Simplicite from 'simplicite';
+import { SimpliciteFS } from './SimpliciteFS';
 
 export function activate(context: vscode.ExtensionContext) {
 	let cfg = vscode.workspace.getConfiguration('simplicite');
@@ -7,19 +8,42 @@ export function activate(context: vscode.ExtensionContext) {
 	let username = cfg.get("username");
 	console.log(`Simplicite tools activated for URL ${url} and username ${username}`);
 
-	let app = Simplicite.session({ url: url, username: username, password: cfg.get("password") });
+	let app = Simplicite.session({ url: url, username: username, password: cfg.get("password"), debug: true });
+	const mdl = app.getBusinessObject('Module');
 
 	context.subscriptions.push(vscode.commands.registerCommand('simplicite.appinfo', () => {
 		app.getAppInfo().then((info: any) => {
-			console.log(JSON.stringify(info));
+			app.debug(JSON.stringify(info));
 			vscode.window.showInformationMessage(`${info.title} ${info.platformversion} (${url})`);
 		});
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand('simplicite.sysinfo', () => {
 		app.getSysInfo().then((info: any) => {
-			console.log(JSON.stringify(info));
+			app.debug(JSON.stringify(info));
 			vscode.window.showInformationMessage(`Object cache: ${info.cacheobject} / ${info.cacheobjectmax}. Process cache: ${info.cacheprocess} / ${info.cacheprocessmax}, Grant cache: ${info.cachegrant} / ${info.cachegrantmax}`);
+		});
+	}));
+
+	const fs = new SimpliciteFS();
+
+	context.subscriptions.push(vscode.workspace.registerFileSystemProvider('simplicite', fs, { isCaseSensitive: true }));
+
+	context.subscriptions.push(vscode.commands.registerCommand('simplicite.initWorkspace', () => {
+		app.getAppInfo().then((info: any) => {
+			app.debug(JSON.stringify(info));
+			vscode.workspace.updateWorkspaceFolders(0, 0, { uri: vscode.Uri.parse('simplicite:/'), name: info.title });
+		});
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('simplicite.refresh', () => {
+		mdl.search({}).then((modules: any) => {
+			app.debug(JSON.stringify(modules));
+			for (let i = 0; i < modules.length; i++) {
+				const module = modules[i];
+				console.log(module.mdl_name);
+				fs.createDirectory(vscode.Uri.parse(`simplicite:/${module.mdl_name}/`));
+			}
 		});
 	}));
 }
